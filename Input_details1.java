a1b8f819f86007ce9287ce5193ec49eb3a503f5d
shreyapatil-16 <patillshreya.16@gmail.com>
2026-08-31 11:17:06 +0530

    Create Input_details1.java

---
+++ Input_details1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Input_details1{
+    public static void main(String args[]){
+        String name;
+        int age;
+        int phoneno;
+        int aadhar;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        name=sc.nextLine();
+        System.out.println("Enter your age");
+        age=sc.nextInt();
+        System.out.println("Enter your phone number");
+        roll_no=sc.nextInt();
+        System.out.println("Enter your aadhar number");
+        marks=sc.nextInt();
+        System.out.println("My name is "+name);
+        System.out.println("My age is "+age);
+        System.out.println("My phone number is "+phoneno);
+        System.out.println("My aadhar number is "+aadhar);
+    }
+    
+}
