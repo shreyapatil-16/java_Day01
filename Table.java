3f89c4f3166bfb17fc639b96e7ba9d92094c5fe3
shreyapatil-16 <patillshreya.16@gmail.com>
2026-08-31 11:24:50 +0530

    Create Table.java

---
+++ Table.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Table {
+    public static void main(String[]args){
+        System.out.println("Enter a number");
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(num+" * "+ i +" = "+ num*i);
+        }
+
+    }   
+}
